c0bfb34549d23e57f82a78b53b25e1e9f5d831ab
Justin <knoxxbox@gmail.com>
2016-12-09 02:32:24 -0500

    Setting up basic account settings

--- resources/assets/js/components/AccountSettings.vue
+++ resources/assets/js/components/AccountSettings.vue
@@ -1,23 +1,79 @@
 <template>
-    <div class="settings">
+    <div>
+        <div class="panel panel-default">
+            <div class="panel-heading">Manage Destiny Account</div>
 
-        <input v-model="gamertag" type="text" placeholder="Enter your gamertag" class="form-control">
-         <label>
-            <input type="radio" value="playstation" v-model="console">
-            Playstation
-        </label>
-        <label>
-            <input type="radio" value="xbox" v-model="console">
-           Xbox 
-        </label>
+            <div class="panel-body">
 
-   </div>
+                <div class="col-xs-4">
+                    <div>
+                        <label>
+                            <input type="radio" value="playstation" v-model="console">
+                            Playstation
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="radio" value="xbox" v-model="console">
+                            Xbox
+                        </label>
+                    </div>
+                </div>
+
+                <div class="col-xs-8">
+                    <input v-model="gamertag" type="text" placeholder="Enter your gamertag" class="form-control">
+                </div>
+
+            </div>
+        </div>
+
+        <br><br><br>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">Manage Account</div>
+
+            <div class="panel-body">
+
+                <div class="col-xs-8">
+                    <input v-model="email" type="text" placeholder="Be sure to enter an email here" class="form-control">
+                </div>
+
+                <div class="col-xs-4">
+                    <div>
+                        <label>
+                            <input type="radio" value="playstation" v-model="console">
+                            Emails Enabled
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            <input type="radio" value="xbox" v-model="console">
+                            Disable Emails
+                        </label>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </template>
-
+<style scoped>
+    .panel-body {
+        padding: 15px;
+        display: flex;
+        align-items: center;
+    }
+</style>
 <script>
     export default {
         mounted() {
             console.log('Component mounted.')
+        },
+        data(){
+            return {
+                console: '',
+                gamertag: ''
+            }
         }
     }
 </script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Manage Your Account</div>
-
-                <div class="panel-body">
-                    <account-settings></account-settings>
-                </div>
-            </div>
+            <account-settings></account-settings>
         </div>
     </div>
 </div>
